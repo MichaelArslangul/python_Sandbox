ea117e3a9b2307227891122b31b740de4083fee1
MIchaelArslangul <michael.arslangul@gmail.com>
2019-05-22 06:35:43 -0400

    Working solution

--- DP/LongestStringChain.py
+++ DP/LongestStringChain.py
@@ -5,16 +5,14 @@ class LongestStringCheck:
 
 
     def longestStrChain(self, words):
-        dp = defaultdict(list)
-        s_words = sorted(words, key = len)
-        for _word in s_words:
+        dp = defaultdict(int)
+        for _word in sorted(words, key = len):
             for i in range(len(_word)):
                 if _word[:i] + _word[i+1:] in dp.keys():
-                    dp[_word] = dp[_word[:i] + _word[i+1:]]
-                    dp[_word].append(_word)
+                    dp[_word] = max(dp[_word], dp[_word[:i] + _word[i+1:]] +1)
                 else:
-                    dp[_word].append(_word)
-        return dp
+                    dp[_word] = max(dp[_word], 1)
+        return max(dp.values())
 
 
 
@@ -22,5 +20,3 @@ lsc = LongestStringCheck()
 input = ["a","b","ba","bca","bda","bdca"]
 print("longest sting check of: {} is: {}".format(
     input, lsc.longestStrChain(input)))
-# ["a","b","ba","bca","bda","bdca"]
-# ["bdca","a","d","bca","ba","bda"]
