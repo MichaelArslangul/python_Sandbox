9d60d83bdacd6e481e28b6fcb90388e79f7f24c7
MIchaelArslangul <michael.arslangul@gmail.com>
2019-07-17 06:33:39 -0400

    Good score on Leetcode

---
+++ CompareVersionNumbers.py
@@ -0,0 +1,21 @@
+from itertools import zip_longest
+class CompareVersionNumbers:
+
+    def compareVersion(self, version1: str, version2: str) -> int:
+        for v1, v2 in zip_longest(map(int, version1.split('.')), map(int, version2.split('.')),
+                                  fillvalue = 0):
+            print(v1, v2)
+            if v1 > v2:
+                return 1
+            elif v1 < v2:
+                return -1
+        return 0
+
+
+
+
+cv = CompareVersionNumbers()
+version1 = "2.1.7.1"
+version2 = "2.1.4"
+print("is version1 <{}>, greater than version2 <{}>: {}".format(version1, version2,
+        cv.compareVersion(version1, version2)))
