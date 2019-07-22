341e5ed09cc2966a60e452fcbc8d7201a8d915e6
MIchaelArslangul <michael.arslangul@gmail.com>
2019-07-21 21:00:55 -0400

    Refactored

--- DP/LongestCommonSubsequence.py
+++ DP/LongestCommonSubsequence.py
@@ -3,16 +3,16 @@ class LongestCommonSubsequence:
     """
 
     def longest_sub(self, array1, array2):
-        M = len(array1)
-        N = len(array2)
-        long_sub = [[0]*(N + 1)]*(M + 1)
-        for i in range(1, M+1):
-            for j in range(1, N+1):
+        m = len(array1)
+        n = len(array2)
+        DP = [[0] * (n + 1) for i in range(m + 1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
                 if array1[i-1] == array2[j-1]:
-                    long_sub[i][j] = 1 + long_sub[i-1][j-1]
+                    DP[i][j] = 1 + DP[i-1][j-1]
                 else:
-                    long_sub[i][j] = max(long_sub[i][j-1], long_sub[i-1][j])
-        return long_sub[M][N]
+                    DP[i][j] = max(DP[i][j-1], DP[i-1][j])
+        return DP[m][n]
 
 
 
